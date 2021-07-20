4e9a61cb3f6a8daf9eadaaad8d645e7c1b2d8c0b
Trent Summerlin <whirvis@ardenus.org>
2021-07-20 01:48:13 -0500

    Add warning comment to fallback() in Config.java

--- src/main/java/net/whirvis/mc/discraft/bot/config/Config.java
+++ src/main/java/net/whirvis/mc/discraft/bot/config/Config.java
@@ -10,6 +10,13 @@ public @interface Config {
 
 	String key();
 
+	/**
+	 * Due to the limitations of annotations in Java, this fallback value is
+	 * forced to be a {@code String}. Keep in mind that even though this is a
+	 * string, the fallback value for a config may be of a different type!
+	 * 
+	 * @return the fallback value in string representation.
+	 */
 	String fallback() default "";
 
 }
